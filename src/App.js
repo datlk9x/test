cfe591ca5ec141a06e43260c355425ccc0477e33
Dat <xekolk123@gmail.com>
2022-07-14 14:31:22 +0700

    test comment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Login from './Pages/Login'
 import ForgetPass from './Pages/ForgetPass'
 import Register from './Pages/Register'
 import Test from './Pages/Test'
-
+// test comment
 const App = () => {
   return (
 	<div>
